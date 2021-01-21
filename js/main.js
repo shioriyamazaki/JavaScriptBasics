976bc7d8d0fb3393c278e8d81b6581ece0bf0c50
shioriyamazaki <72192957+shioriyamazaki@users.noreply.github.com>
2021-01-21 09:59:17 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function showAd() {
+  console.log('----------');
+  console.log('--- Ad ---');
+  console.log('----------');
+}
+
+showAd();
+console.log('Tom is great!');
+console.log('Bob is great!');
+showAd();
+console.log('Steve is great!');
+console.log('Richard is great!');
+showAd();
